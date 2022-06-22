b77994ffc17df7fea58d88d40b69e1a62aea0422
Igor <eliseev88@gmail.com>
2022-06-22 12:13:37 +0300

    add validation to feedback

--- back/laravel/resources/views/components/feedback.blade.php
+++ back/laravel/resources/views/components/feedback.blade.php
@@ -5,9 +5,9 @@
                 <h4>Связатся с менеджером</h4>
                 <p>Заполните форму и менеджер свяжется с вами в ближайшее время</p>
 
-                <input type="text" placeholder="Ваше имя" name="name" id="name" class="feedback-input">
-                <input class="feedback-input" type="number" placeholder="Ваш номер телефона" name="phone" id="phone">
-                <input class="feedback-input" type="email" placeholder="Ваш email" name="email" id="email">
+                <input type="text" placeholder="Ваше имя" name="name" id="name" class="feedback-input" required>
+                <input class="feedback-input" type="text" placeholder="Ваш номер телефона" name="phone" id="phone" required>
+                <input class="feedback-input" type="email" placeholder="Ваш email" name="email" id="email" required>
 
                 <button class="feedback-btn" type="submit" id="send-feedback">Отправить</button>
 
